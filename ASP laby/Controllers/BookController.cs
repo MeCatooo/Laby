20abe5dff1abbf027869013d683979793cae79a3
MeCatooo <79982959+MeCatooo@users.noreply.github.com>
2021-10-15 14:54:55 +0200

    koniec labów 15.10

---
+++ ASP laby/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using ASP_laby.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_laby.Controllers
+{
+    public class BookController : Controller
+    {
+        static List<Book> books = new List<Book>();
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+        public IActionResult AddForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Book Book)
+        {
+            books.Add(Book);
+            return View("ConfirmBook", Book);
+        }
+        public IActionResult List()
+        {
+            return View(books);
+        }
+    }
+}
